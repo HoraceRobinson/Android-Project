63e459b90a77db5abe4ec49abc90f298c0c1199c
HoranceRoinson <3386875092@qq.com>
2022-06-14 10:58:35 +0800

    形界面优化

--- app/src/main/java/com/example/ticketpurchase/PatternFragment.java
+++ app/src/main/java/com/example/ticketpurchase/PatternFragment.java
@@ -1,26 +1,47 @@
 package com.example.ticketpurchase;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.example.ticketpurchase.room.XingIdiom;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PatternFragment extends Fragment {
 
+    List<Button> btnList = new ArrayList<>();
+    public XingIdiom xingIdiom;
 
     public PatternFragment() {
         // Required empty public constructor
     }
 
-    // TODO: Rename and change types and number of parameters
+
     public static PatternFragment newInstance() {
         PatternFragment fragment = new PatternFragment();
         return fragment;
@@ -36,69 +57,131 @@ public class PatternFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pattern, container, false);
 
-        Button btn1 = view.findViewById(R.id.btn1);
-        Button btn2 = view.findViewById(R.id.btn2);
-        Button btn3 = view.findViewById(R.id.btn3);
-        Button btn4 = view.findViewById(R.id.btn4);
-        btn1.setText("振聋发聩");
-        btn2.setText("震聋发聩");
-        btn3.setText("振聋发溃");
-        btn4.setText("震聋发溃");
+        init(view);
+        setContent();
         TextView textView = view.findViewById(R.id.text_explain);
         LinearLayout linearLayout = view.findViewById(R.id.next_btn);
-        btn1.setOnClickListener(view14 -> {
-            btn1.setBackground(getResources().getDrawable(R.drawable.background_green));
-            btn1.setTextColor(getResources().getColor(R.color.white));
-            if(textView.getText() == ""){
-                textView.setText("回答正确！");
-            }
-        });
 
-        btn2.setOnClickListener(view13 -> {
-            btn2.setBackground(getResources().getDrawable(R.drawable.background_red));
-            btn2.setTextColor(getResources().getColor(R.color.white));
-            if(textView.getText() == ""){
-                textView.setText("回答错误！");
-            }
-        });
+        for(Button btn : btnList){
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(btn.getText() == xingIdiom.getCorrect()){
+                        btn.setBackground(getResources().getDrawable(R.drawable.background_green));
+                        btn.setTextColor(getResources().getColor(R.color.white));
+                        if(textView.getText() == ""){
+                            textView.setText("回答正确！");
+                        }
+                    }
+                    else{
+                        btn.setBackground(getResources().getDrawable(R.drawable.background_red));
+                        btn.setTextColor(getResources().getColor(R.color.white));
+                        if(textView.getText() == ""){
+                            textView.setText("回答错误！");
+                        }
+                    }
+                }
+            });
+        }
 
-        btn3.setOnClickListener(view1 -> {
-            btn3.setBackground(getResources().getDrawable(R.drawable.background_red));
-            btn3.setTextColor(getResources().getColor(R.color.white));
-            if(textView.getText() == ""){
-                textView.setText("回答错误！");
-            }
-        });
-
-        btn4.setOnClickListener(view12 -> {
-            btn4.setBackground(getResources().getDrawable(R.drawable.background_red));
-            btn4.setTextColor(getResources().getColor(R.color.white));
-            if(textView.getText() == ""){
-                textView.setText("回答错误！");
-            }
-        });
 
         linearLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btn1.setText("浑金璞玉");
-                btn2.setText("混金璞玉");
-                btn3.setText("浑金镤玉");
-                btn4.setText("混金镤玉");
-                btn1.setTextColor(getResources().getColor(R.color.black));
-                btn2.setTextColor(getResources().getColor(R.color.black));
-                btn3.setTextColor(getResources().getColor(R.color.black));
-                btn4.setTextColor(getResources().getColor(R.color.black));
-                btn1.setBackground(getResources().getDrawable(R.drawable.white_background));
-                btn2.setBackground(getResources().getDrawable(R.drawable.white_background));
-                btn3.setBackground(getResources().getDrawable(R.drawable.white_background));
-                btn4.setBackground(getResources().getDrawable(R.drawable.white_background));
-                textView.setText("");
+                setContent();
             }
         });
 
-        // Inflate the layout for this fragment
         return view;
+    }
+
+    public void init(View view){
+        btnList.add(view.findViewById(R.id.btn1));
+        btnList.add(view.findViewById(R.id.btn2));
+        btnList.add(view.findViewById(R.id.btn3));
+        btnList.add(view.findViewById(R.id.btn4));
+        for (Button btn: btnList) {
+            btn.setTextColor(getResources().getColor(R.color.black));
+            btn.setBackground(getResources().getDrawable(R.drawable.white_background));
+        }
+    }
+
+    public void setContent(){
+        PatternTask task = new PatternTask((MainActivity) getActivity());
+        task.execute();
+    }
+
+    private static class PatternTask extends AsyncTask<Void, Void, String> {
+
+        public static final String URL = "http://47.113.102.111:8080/getOneRandomXingIdiom";
+        private final WeakReference<MainActivity> activityReference;
+
+        public PatternTask  (MainActivity context) {
+            activityReference = new WeakReference<>(context);
+        }
+
+        @Override
+        protected String doInBackground(Void ...voids) {
+            InputStream inputStream = null;
+            InputStreamReader inputStreamReader = null;
+            BufferedReader bufferedReader = null;
+            String tmp;
+            StringBuilder stringBuilder = new StringBuilder();
+            try {
+                java.net.URL url = new URL(URL);
+                URLConnection connection = url.openConnection();
+                connection.connect();
+                inputStream = connection.getInputStream();
+                inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+                bufferedReader = new BufferedReader(inputStreamReader);
+                while ((tmp = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(tmp);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (inputStreamReader != null) {
+                    try {
+                        inputStreamReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (bufferedReader != null) {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return stringBuilder.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String str) {
+            MainActivity activity = activityReference.get();
+            if (activity == null || activity.isFinishing()) {
+                Log.e("PatternFragment","Activity弱引用创建失败或Activity已经结束");
+                return;
+            }
+            JSONObject data = JSON.parseObject(str,JSONObject.class);
+            ViewPager viewpager = activity.findViewById(R.id.view_pager1);
+            PatternFragment fragment = (PatternFragment) viewpager.getAdapter().instantiateItem(viewpager, 0);
+
+            fragment.xingIdiom = (XingIdiom) data.get("idiom");
+            String content = data.getString("content");
+            List<String> list = JSONObject.parseArray(content,String.class);
+
+            System.out.println(list);
+        }
     }
 
     @Override
